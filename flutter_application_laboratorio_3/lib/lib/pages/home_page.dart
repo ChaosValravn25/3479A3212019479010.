954e654b8f7ddc632f220b0b7466fac214d57d79
ChaosValravn25 <silver.wolf1@hotmail.com>
2025-04-16 22:10:54 -0400

    page

---
+++ flutter_application_laboratorio_3/lib/lib/pages/home_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Página Principal")),
+      body: Center(child: Text("¡Bienvenido a Flutter!")),
+    );
+  }
+}
