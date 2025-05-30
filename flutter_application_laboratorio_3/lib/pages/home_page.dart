0c46ae0e73211b9315b4b9265b634f4f3ed71ae4
ChaosValravn25 <silver.wolf1@hotmail.com>
2025-05-30 13:15:14 -0400

    avances_del_lab_8

--- flutter_application_laboratorio_3/lib/pages/home_page.dart
+++ flutter_application_laboratorio_3/lib/pages/home_page.dart
@@ -7,11 +7,10 @@ import 'package:provider/provider.dart';
 import 'package:flutter_application_laboratorio_3/provider/app_data.dart';
 import 'package:flutter_application_laboratorio_3/pages/preferences_page.dart';
 import 'package:flutter_application_laboratorio_3/pages/activity_page.dart';
+import 'package:http/http.dart' as http;
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required String title});
   
-
-
 @override
   State<MyHomePage> createState() {
     
@@ -29,6 +28,23 @@ class _HomePageState extends State<MyHomePage> {
   void initState() {
     super.initState();
     
+  }
+
+  int _counter = 0;
+  String _imageUrl = 'https://picsum.photos/250?image=10'; // Valor inicial
+
+  Future<void> _getNewImage() async {
+    final newImageUrl = 'https://picsum.photos/250?image=$_counter';
+    try {
+      final response = await http.head(Uri.parse(newImageUrl));
+      if (response.statusCode == 200) {
+        setState(() => _imageUrl = newImageUrl);
+      } else {
+        setState(() => _imageUrl = '');
+      }
+    } catch (e) {
+      setState(() => _imageUrl = '');
+    }
   }
 
   @override
@@ -59,6 +75,10 @@ class _HomePageState extends State<MyHomePage> {
   void reassemble() {
     super.reassemble();
     
+  }
+
+  void _incrementCounter() {
+    setState(() => _counter++);
   }
 
   @override
@@ -109,11 +129,32 @@ class _HomePageState extends State<MyHomePage> {
             ),
             child: const Text("Actividades"),
             ),
-
-
+  
+             Text("Contador: $_counter"),
+            const SizedBox(height: 10),
+            Image.network(
+              _imageUrl.isNotEmpty ? _imageUrl : '',
+              width: 250,
+              height: 250,
+              fit: BoxFit.cover,
+              errorBuilder: (context, error, stackTrace) {
+                return const Text("No se pudo cargar la imagen", style: TextStyle(color: Colors.red));
+              },
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: _getNewImage,
+              child: const Text("Actualizar imagen"),
+            ),
+            ElevatedButton(
+              onPressed: _incrementCounter,
+              child: const Text("Aumentar contador"),
+            ),
+            
           ],
         ),
       ),
+    
     );
   }
 }
